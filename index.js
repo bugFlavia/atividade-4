8c729b388c407f497f75d992d37d110dd2ffbad7
flavianunes2 <101286261+bugFlavia@users.noreply.github.com>
2022-03-09 18:59:06 +0000

    criei mais um objeto

--- index.js
+++ index.js
@@ -16,7 +16,10 @@ class Retangulo{
 
 let quadrado = new Retangulo("10", "10");
 let retangulo = new Retangulo ("30", "60");
+let aleatorio = new Retangulo ("10", "20");
+
            
 console.log(quadrado.area());
 console.log(retangulo.area());
+console.log(aleatorio.area());
 
